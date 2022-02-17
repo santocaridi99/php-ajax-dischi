9f629de7f901f9a8d2f563fc26bf4c0104abaeb0
santocaridi99 <santocaridi99@gmail.com>
2022-02-17 20:24:47 +0100

    aggiunto una select per il dischiVue

--- js/script.js
+++ js/script.js
@@ -2,12 +2,25 @@ new Vue({
     el: "#app",
     data: {
         // dichiaro un array di dischi vuoto che riempirò con chiamata axios
-        discs: []
+        discs: [],
+        // dichiaro array di dischi filtrati
+        filteredDisc: [],
+        // variabile genere selezionato (cambiera al change della select)
+        selectedGenre: '',
     },
     mounted() {
         axios.get("http://localhost:81/php-ajax-dischi/api/DischiApi.php").then((res) => {
             // riempio array disc
             this.discs = res.data;
-        })
-    }
+            this.filteredDisc = res.data;
+        });
+    },
+    methods: {
+        genreFilter() {
+            this.filteredDisc = this.discs.filter((disc) => {
+                return disc.genre === this.selectedGenre || this.selectedGenre === '';
+            });
+        },
+
+    },
 })
