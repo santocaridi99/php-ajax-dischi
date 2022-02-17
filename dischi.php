18ecd9c9ae05a71f6ff95ab36f6cfb4671b8015a
santocaridi99 <santocaridi99@gmail.com>
2022-02-17 17:08:26 +0100

    (Milestone 1) stampato tramite il php le cards dei dischi

---
+++ dischi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dischi.php</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <!-- stampo i dischi solamente con php -->
+        <!-- nel container passerò con inlcude cartella Cardbox presente nel template -->
+        <div class="main-container">
+            <?php include __DIR__ . "/template/CardBox.php" ?>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
